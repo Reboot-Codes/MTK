f927a483755371e9a6a2def88d380ce3461fa53d
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2020-09-07 08:50:05 -0700

    Create Hello-World.ino

---
+++ DIS91/Hello-World.ino
@@ -0,0 +1,18 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
+}
+
+void loop() {
+  
+}
